3c77a252da830a81d9f51a10357b46c733659773
Кирилл <kirill.denisenko.01@gmail.com>
2023-08-06 15:25:53 +0300

    Fix

--- src/components/parts/PositionLimiter/PositionLimiter.tsx
+++
@@ -1,8 +0,0 @@
-import {FC, PropsWithChildren} from 'react';
-import './PositionLimiter.style.css';
-
-const PositionLimiter: FC<PropsWithChildren> = ({ children }) => {
-  return <div className={'container'}>{children}</div>;
-};
-
-export {PositionLimiter};
--- src/hooks/useAuth.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext, TAuthContextInitial } from '../hoc/AuthProvider';
-
-export function useAuth(): TAuthContextInitial {
-  return useContext<TAuthContextInitial>(AuthContext);
-}
